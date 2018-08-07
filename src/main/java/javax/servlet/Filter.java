b3aaa1de7ab58d3187c8b32b823c68db74654aca
jacky_glo <jacky_glo@jacky.com>
2018-08-07 09:52:00 +0800

    first commit

--- src/main/java/javax/servlet/Filter.java
+++ src/main/java/javax/servlet/Filter.java
@@ -20,19 +20,16 @@ package javax.servlet;
 import java.io.IOException;
 
 /**
- * <p>A filter is an object that performs 
- * filtering tasks on either the request to a resource (a servlet or static content), or on the response
- * from a resource, or both.</p>
- * 
- * <p>Filters perform filtering in the <code>doFilter</code> method.
+ * A filter is an object that performs  filtering tasks on either the request to a resource
+ * (a servlet or static content), or on the response
+ * from a resource, or both.
+ * Filters perform filtering in the <code>doFilter</code> method.
  * Every Filter has access to a FilterConfig object from which it can obtain
  * its initialization parameters, and a reference to the ServletContext which
  * it can use, for example, to load resources needed for filtering tasks.
- *
- * <p>Filters are configured in the deployment descriptor of a web
- * application.
- *
- * <p>Examples that have been identified for this design are:
+ * 过滤器需要声明在配置配置文件中
+ * Filters are configured in the deployment descriptor of a web application.
+ * Examples that have been identified for this design are:
  * <ol>
  * <li>Authentication Filters
  * <li>Logging and Auditing Filters
@@ -50,40 +47,35 @@ import java.io.IOException;
 
 public interface Filter {
 
-    /** 
+    /**
+     * 被容器调用，传递进一个 FilterConfig 用于初始化此servlet
      * <p>Called by the web container
      * to indicate to a filter that it is being placed into service.</p>
-     *
+     * <p>
      * <p>The servlet container calls the init
      * method exactly once after instantiating the filter. The init
      * method must complete successfully before the filter is asked to do any
      * filtering work.</p>
-     * 
+     * <p>
      * <p>The web container cannot place the filter into service if the init
      * method either</p>
      * <ol>
      * <li>Throws a ServletException
      * <li>Does not return within a time period defined by the web container
      * </ol>
-     * 
-     * @implSpec
-     * The default implementation takes no action.
-     *
-     * @param filterConfig a <code>FilterConfig</code> object containing the
-     *                     filter's configuration and initialization parameters 
-     * @throws ServletException if an exception has occurred that interferes with
-     *                          the filter's normal operation
      */
-    default public void init(FilterConfig filterConfig) throws ServletException {}
-	
-	
+    default public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+
     /**
+     * 被容器调用，执行过滤方法 通过过滤器链来进行传递
      * The <code>doFilter</code> method of the Filter is called by the
      * container each time a request/response pair is passed through the
      * chain due to a client request for a resource at the end of the chain.
      * The FilterChain passed in to this method allows the Filter to pass
      * on the request and response to the next entity in the chain.
-     *
+     * <p>
      * <p>A typical implementation of this method would follow the following
      * pattern:
      * <ol>
@@ -104,15 +96,6 @@ public interface Filter {
      * <li>Directly set headers on the response after invocation of the
      * next entity in the filter chain.
      * </ol>
-     *
-     * @param request the <code>ServletRequest</code> object contains the client's request
-     * @param response the <code>ServletResponse</code> object contains the filter's response
-     * @param chain the <code>FilterChain</code> for invoking the next filter or the resource
-     * @throws IOException if an I/O related error has occurred during the processing
-     * @throws ServletException if an exception occurs that interferes with the
-     *                          filter's normal operation
-     *
-     * @see UnavailableException
      */
     public void doFilter(ServletRequest request, ServletResponse response,
                          FilterChain chain)
@@ -120,22 +103,23 @@ public interface Filter {
 
 
     /**
-     * <p>Called by the web container 
+     * 销毁时，被容器调用
+     * <p>Called by the web container
      * to indicate to a filter that it is being
      * taken out of service.</p>
-     *
+     * <p>
      * <p>This method is only called once all threads within the filter's
      * doFilter method have exited or after a timeout period has passed.
      * After the web container calls this method, it will not call the
      * doFilter method again on this instance of the filter.</p>
-     *
+     * <p>
      * <p>This method gives the filter an opportunity to clean up any
      * resources that are being held (for example, memory, file handles,
      * threads) and make sure that any persistent state is synchronized
      * with the filter's current state in memory.</p>
-     * 
-     * @implSpec
-     * The default implementation takes no action.
+     *
+     * @implSpec The default implementation takes no action.
      */
-    default public void destroy() {}
+    default public void destroy() {
+    }
 }
